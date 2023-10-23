57e96f38ca616d96d6238ae4c1df471c94efb9bf
ZakharTsarapov <tsarapovs@gmail.com>
2023-10-23 18:01:53 +0300

    bot done

--- 3_CLI_telegram_console_sender/index.js
+++ 3_CLI_telegram_console_sender/index.js
@@ -4,8 +4,14 @@ import TelegramBot from "node-telegram-bot-api";
 const program = new Command();
 const token = "6630033932:AAEhL-gfN0sIEES5_dOfDQ14SSwINKpG5DM";
 
-const bot = new TelegramBot(token, { polling: true });
-const chatId = msg.chat.id;
+const bot = new TelegramBot(token, { polling: false });
+
+bot.on('message', msg => {
+    const {chat: {id}} = msg
+    bot.sendMessage(id, "pong")
+})
+
+
 
 program
   .name("string-util")
@@ -14,8 +20,20 @@ program
 
 program
   .command("send-message")
+  .alias("m")
   .description("send message to bot")
   .argument("<string>", "your message")
   .action((str) => {
-    bot.sendMessage(chatId, str);
+    bot.sendMessage(560142779, str);
   });
+
+program
+.command("send-photo")
+.alias("p")
+.description("send photo to bot")
+.argument("<photo>", "your photo")
+.action((photo) => {
+    bot.sendPhoto(560142779, photo)
+})
+
+  program.parse();  
